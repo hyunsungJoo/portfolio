7d55f410f72bec282e2e6eb12d3a77451ac54038
주현성 <joo@juhyeonseong-ui-MacBookPro.local>
2024-07-18 02:14:40 +0900

    [Modify] app.vue background color

--- src/App.vue
+++ src/App.vue
@@ -41,6 +41,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  background-color: #000000;
   color: #2c3e50;
   margin-top: 60px;
 }
